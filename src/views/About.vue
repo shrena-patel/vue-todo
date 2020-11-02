324bfd08b3f2821d3c7f9372d9d3c8b0c62c1afd
Shrena Patel <shrena.p@hotmail.co.uk>
2020-11-02 15:44:48 +1300

    minor styling changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="about">
     <h1>About</h1>
-    <p>This is the TodoList App built with Vue</p>
+    <p class="info">This is the a todo list app built with the help of <a href="https://www.youtube.com/watch?v=Wy9q22isx3U">Traversy Media.</a></p>
   </div>
 </template>
+
+<style scoped>
+  .about {
+    text-align: center;
+    margin-top: 20px;
+  }
+
+  .info a {
+    color:lightseagreen;
+    text-decoration: none;
+  }
+</style>
